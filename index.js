47a4e15a611085bb73e6038c5112164f3f7c3208
mahdimortuza <mahdi.webx@gmail.com>
2023-10-04 11:19:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+// e-commerceUser
+// JQe7gwfjYDQIYJFZ
+
+const uri = process.env.URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+
+    await client.connect();
+    const productsCollection = client.db("e_commerceDb");
+    const products = productsCollection.collection("products");
+
+    // Send a ping to confirm a successful connection
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+//   root api for test
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`e commerce is open on port ${port}`);
+});
