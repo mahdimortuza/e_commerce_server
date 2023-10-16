9a6dd19059cad0ad222ccc4811ff0058888a2043
mahdimortuza <mahdi.webx@gmail.com>
2023-10-16 14:55:35 +0600

    error solved

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(cors());
 
 const uri = process.env.URI;
 
+
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
