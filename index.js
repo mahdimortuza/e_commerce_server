f8ccd928421596f874777ad4ee46ffa35a663b6d
mahdimortuza <mahdi.webx@gmail.com>
2023-10-17 20:24:38 +0600

    index file updated

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.use(cors());
 
 const uri = process.env.URI;
 
-
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
